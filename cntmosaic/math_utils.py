26952bc0ce7c096c4e53ffe9376566f084fbd86b
Shozen Dan <xiaoran.duan@gmail.com>
2024-11-20 22:17:45 +0000

    [update] refactor code

---
+++ cntmosaic/math_utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+import jax
+import jax.numpy as jnp
+from jax import Array
+from jax.typing import ArrayLike
+
+def closure(x: ArrayLike, axis: int=0) -> Array:
+  """Closure operation for a given axis"""
+  return x / jnp.sum(x, axis=axis, keepdims=True)
+
+def aitchison_orthnorm_basis(d: int) -> Array:
+  """Aitchison orthonormal basis"""
+  U = np.zeros((d, d-1))
+  for i in range(d-1):
+    j = i + 1
+    U[:j,i] = np.sqrt(1/(j*(j+1)))
+    U[i+1,i] = -np.sqrt(1/(j+1))
+    
+  return closure(np.exp(U))
+
+def alr(x: ArrayLike, axis: int=0) -> Array:
+  """Additive log ratio transformation"""
+  denom = 1 - jnp.sum(x, axis=axis, keepdims=True)
+  return jnp.log(x / denom)
+  
+def clr(x: ArrayLike, axis: int=0) -> Array:
+  """Centered log ratio transformation"""
+  return jnp.log(x) - jnp.mean(jnp.log(x), axis=axis, keepdims=True)
+
+def ilr(x: ArrayLike, axis: int=0) -> Array:
+  """Isometric log ratio transformation"""
+  shape = list(x.shape)
+  U = aitchison_orthnorm_basis(shape[axis])
+  y = clr(x, axis=axis)
+  return jnp.apply_along_axis(lambda y: jnp.matmul(U.T, y), axis=axis, arr=y)
+  
+def inverse_alr(x: ArrayLike, axis: int=0) -> Array:
+  """Inverse additive log ratio transformation"""
+  shape = list(x.shape)
+  ones_shape = shape.copy()
+  ones_shape[axis] = 1
+  ones = jnp.ones(ones_shape)
+  
+  x = jnp.concatenate((jnp.exp(x), ones), axis=axis)
+  return closure(x, axis=axis)
+  
+def inverse_clr(x: ArrayLike, axis: int=0) -> Array:
+  """Inverse centered log ratio transformation"""
+  return jax.nn.softmax(x, axis=axis)
+
+def inverse_ilr(x: ArrayLike, axis: int=0) -> Array:
+  """Inverse isometric log ratio transformation"""
+  shape = list(x.shape)
+  U = aitchison_orthnorm_basis(shape[axis]+1)
+  Uz = jnp.apply_along_axis(lambda z: jnp.matmul(U, z), axis=axis, arr=x)
+  
+  return jax.nn.softmax(jnp.exp(Uz), axis=axis)
+
+def log_inverse_ilr(x: ArrayLike, axis: int=0) -> Array:
+  """Log inverse isometric log ratio transformation"""
+  return jnp.log(inverse_ilr(x, axis=axis))
---
+++ cntmosaic/model_utils.py
@@ -0,0 +1,90 @@
+import numpy as np
+import jax.numpy as jnp
+from jax import Array
+from jax.typing import ArrayLike
+from numpy.typing import NDArray
+from statsmodels.gam.smooth_basis import BSplines
+
+def non_nuisance_grid(A: int) -> NDArray:
+    """
+    Returns the coordinates of the non-nuisance entries of a 
+    difference-in-age by age matrix of size (2A - 1 x A) in column-major order.
+
+    :param A: number of age groups
+    """
+    return np.array([[A - i + j, i + 1] for i in range(A) for j in range(A)])
+
+
+def symmetrize_from_lower_tri(N: int) -> NDArray:
+    """
+    Indices to symmetrize a vector containing the elements of a 
+    lower triangular matrix in column-major order to a vector containing
+    the elements of a symmetric matrix in column-major order
+
+    :param N: size of the square matrix
+    """
+    idx = np.empty(shape=(N**2,), dtype=int)
+    n = 1
+
+    for j in range(N):
+        for i in range(N):
+            if i >= j:
+                idx[n-1] = i + j*N - (j+1)*j/2
+            else:
+                idx[n-1] = j + i*N - (i+1)*i/2
+            n += 1
+
+    return idx
+
+
+def lower_tri_indices(N: int) -> NDArray:
+    """
+    Indices to extract the lower triangular elements of a square matrix
+    in column-major order
+
+    :param N: size of one dimension of the square matrix
+    """
+    idx = np.empty((N*(N+1)//2,), dtype=int)
+    n = 0
+    for j in range(N):
+        for i in range(N):
+            if i >= j:
+                idx[n] = i + j*N
+                n += 1
+
+    return idx
+
+def transpose_vector_indices(rows: int, cols: int) -> NDArray:
+    """
+        Computes the indices to rearrange a vector containing the elements of a matrix in column-major order
+        such that when the matrix is reconstructed, it is the transpose of the original matrix.
+        
+        :param rows: The number of rows in the original matrix.
+        :param cols: The number of columns in the original matrix.
+        :return: An array of indices to rearrange the vector.
+        """
+    original_indices = np.arange(rows * cols).reshape((rows, cols), order='F')
+    transposed_indices = original_indices.T.flatten(order='F')
+    return transposed_indices
+
+def to_square_matrix(x, N: int):
+    return x.reshape(N, N, order='')
+
+def bspline_basis(x: NDArray,
+                  df: int=30,
+                  degree: int=3,
+                  include_intercept: bool=False):
+    bspline = BSplines(x, df=df, degree=degree, include_intercept=include_intercept)
+    bspline_basis = bspline.basis
+    PHI = np.kron(bspline_basis, bspline_basis)
+    
+    return PHI
+
+def mvlogistic(x: ArrayLike, axis: int) -> Array:
+    u = jnp.exp(x) / (1 + jnp.sum(jnp.exp(x), axis=axis, keepdims=True))
+    v = 1 - jnp.sum(u, axis=axis, keepdims=True)
+    
+    return jnp.concatenate([u, v], axis=axis)
+
+def log_mvlogistic(x: ArrayLike, axis: int) -> Array:
+    return jnp.log(mvlogistic(x, axis))

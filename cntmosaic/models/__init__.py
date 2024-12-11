e343d159ee193681dff772aa14df41bd79f6c085
Shozen Dan <xiaoran.duan@gmail.com>
2024-12-11 00:36:15 +0000

    [add] HiBRCrefine

--- cntmosaic/models/__init__.py
+++ cntmosaic/models/__init__.py
@@ -2,10 +2,12 @@ from ._BRC import BRC
 from ._BRCfine import BRCfine
 from ._BRCrefine import BRCrefine
 from ._HiBRCfine import HiBRCfine
+from ._HiBRCrefine import HiBRCrefine
 
 __all__ = [
     'BRC',
     'BRCfine',
     'BRCrefine',
-    'HiBRCfine'
+    'HiBRCfine',
+    'HiBRCrefine'
 ]

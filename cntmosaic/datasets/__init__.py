19299d08fe8293bb03077d60631bc851d9cb92a8
Shozen Dan <xiaoran.duan@gmail.com>
2024-12-11 22:53:54 +0000

    [add] German POLYMOD dataset

---
+++ cntmosaic/datasets/__init__.py
@@ -0,0 +1,3 @@
+from ._base import load_polymod_germany
+
+__all__ = ['load_polymod_germany']
---
+++ cntmosaic/datasets/_base.py
@@ -0,0 +1,25 @@
+from importlib import resources
+import pickle
+
+def load_pickle_data(data_file_name):
+  """Loads `data_file_name` from the package's data directory."""
+  
+  data_path = resources.files('cntmosaic.datasets.data') / data_file_name
+  with data_path.open('rb') as pickle_file:
+    return pickle.load(pickle_file)
+  
+def load_polymod_germany():
+  """Loads the German Polymod dataset.
+  
+  This function loads a cleaned version of the German POLYMOD dataset.
+  
+  Returns
+  -------
+  dict
+      A dictionary with the following
+      - 'contacts': a pandas DataFrame with the contact data
+      - 'participants': a pandas DataFrame with the participant data
+      - 'population': a pandas DataFrame with the population data
+  """
+  
+  return load_pickle_data('polymod_germany.pkl')

5ce13fe8b6cdd4b5e69ae22be21d11c50a539212
Shozen Dan <shozen.dan@keio.jp>
2025-11-13 10:43:12 +0000

    [update] add new data containers

---
+++ cntmosaic/dataloader/containers/__init__.py
@@ -0,0 +1,11 @@
+from ._ContactData import ContactData
+from ._ParticipantData import ParticipantData
+from ._PopulationData import PopulationData
+from ._StratPropData import StratPropData
+
+__all__ = [
+    "ContactData",
+    "ParticipantData",
+    "PopulationData",
+    "StratPropData",
+]

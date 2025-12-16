5dc14bb93c1720b92b5a14ab5289487633d2373b
Shozen Dan <shozen.dan@keio.jp>
2025-12-16 20:46:04 +0000

    [update] Refine preprocessing and utility functions

--- cntmosaic/preprocess/_preprocess.py
+++ cntmosaic/preprocess/_preprocess.py
@@ -1,6 +1,7 @@
 import warnings
-import pandas as pd
+
 import numpy as np
+import pandas as pd
 
 from ._utils import make_full_grid
 
@@ -14,16 +15,21 @@ def convert_to_categorical(data: pd.DataFrame, cols: list[str]) -> pd.DataFrame:
 
 def document_categories(data: pd.DataFrame) -> dict:
     """Document the categories of categorical columns."""
-    return {col: data[col].cat.categories for col in data.select_dtypes(include='category').columns}
+    return {
+        col: data[col].cat.categories
+        for col in data.select_dtypes(include="category").columns
+    }
 
 
-def impute_age_min_max(data: pd.DataFrame,
-                       age_col: str,
-                       age_min_col: str,
-                       age_max_col: str,
-                       type: str = 'midpoint',
-                       dropna: bool = False,
-                       remove_min_max_col: bool = True) -> pd.DataFrame:
+def impute_age_min_max(
+    data: pd.DataFrame,
+    age_col: str,
+    age_min_col: str,
+    age_max_col: str,
+    type: str = "midpoint",
+    dropna: bool = False,
+    remove_min_max_col: bool = True,
+) -> pd.DataFrame:
     """
     Impute missing values in an age column based on the average of corresponding
     minimum and maximum age columns and optionally clean up the DataFrame.
@@ -82,26 +88,31 @@ def impute_age_min_max(data: pd.DataFrame,
     After imputation, if 'dropna' is True and 'age_col' still contains NaN, such rows will be dropped,
     potentially reducing the size of the returned DataFrame.
     """
-    if type not in ['midpoint', 'random']:
+    if type not in ["midpoint", "random"]:
         raise ValueError('type must be either "midpoint" or "random"')
 
     data = data.copy()
 
-    if type == 'midpoint':
-        data[age_col] = np.where(data[age_col].isna(),
-                                 (data[age_min_col] + data[age_max_col]) // 2,
-                                 data[age_col])
-    elif type == 'random':
-        data[age_col] = np.where(data[age_col].isna(),
-                                 np.random.randint(
-                                    data[age_min_col], data[age_max_col] + 1),
-                                 data[age_col])
+    if type == "midpoint":
+        data[age_col] = np.where(
+            data[age_col].isna(),
+            (data[age_min_col] + data[age_max_col]) // 2,
+            data[age_col],
+        )
+    elif type == "random":
+        data[age_col] = np.where(
+            data[age_col].isna(),
+            np.random.randint(data[age_min_col], data[age_max_col] + 1),
+            data[age_col],
+        )
 
     if dropna:
         n0 = data.shape[0]
         data = data.dropna(subset=[age_col])
         n1 = data.shape[0]
-        warnings.warn(f'Dropped {n0 - n1} rows with missing values in {age_col}', UserWarning)
+        warnings.warn(
+            f"Dropped {n0 - n1} rows with missing values in {age_col}", UserWarning
+        )
 
     if remove_min_max_col:
         data = data.drop(columns=[age_min_col, age_max_col], axis=1)
@@ -109,9 +120,9 @@ def impute_age_min_max(data: pd.DataFrame,
     return data
 
 
-def make_train_data(data: pd.DataFrame,
-                    id_var: str,
-                    grp_vars: str | list[str] | None = None) -> pd.DataFrame:
+def make_train_data(
+    data: pd.DataFrame, id_var: str, strat_vars: str | list[str] | None = None
+) -> pd.DataFrame:
     """
     Prepare training data for Bayesian Rate Consistency model.
 
@@ -121,7 +132,7 @@ def make_train_data(data: pd.DataFrame,
         Input data containing necessary columns.
     id_var : str
         Name of the column containing unique individual identifiers.
-    grp_vars : str or list[str], optional
+    strat_vars : str or list[str], optional
         Additional grouping variables for stratification, by default None.
 
     Returns
@@ -143,26 +154,29 @@ def make_train_data(data: pd.DataFrame,
     data = data.copy()
 
     # Add a column 'y' if it does not exist
-    if 'y' not in data.columns:
-        warnings.warn('No column "y" found. Assuming each row represents a single contact.', RuntimeWarning)
-        data['y'] = 1
+    if "y" not in data.columns:
+        warnings.warn(
+            'No column "y" found. Assuming each row represents a single contact.',
+            RuntimeWarning,
+        )
+        data["y"] = 1
 
-    if 'age_cnt' in data.columns:
-        age_vars = ['age_part', 'age_cnt']
-    elif 'age_grp_cnt' in data.columns:
-        age_vars = ['age_part', 'age_grp_cnt']
+    if "age_cnt" in data.columns:
+        age_vars = ["age_part", "age_cnt"]
+    elif "age_grp_cnt" in data.columns:
+        age_vars = ["age_part", "age_grp_cnt"]
     else:
         raise ValueError('data must contain a column "age_cnt" or "age_grp_cnt"')
 
-    if grp_vars is None:
-        selected_vars = [id_var] + age_vars + ['y']
-        grp_vars_part = ['age_part']
-        grp_vars_cnt = age_vars
+    if strat_vars is None:
+        selected_vars = [id_var] + age_vars + ["y"]
+        strat_vars_part = ["age_part"]
+        strat_vars_cnt = age_vars
     else:
-        grp_vars = [grp_vars] if isinstance(grp_vars, str) else grp_vars
-        selected_vars = [id_var] + age_vars + grp_vars + ['y']
-        grp_vars_part = ['age_part'] + grp_vars
-        grp_vars_cnt = age_vars + grp_vars
+        strat_vars = [strat_vars] if isinstance(strat_vars, str) else strat_vars
+        selected_vars = [id_var] + age_vars + strat_vars + ["y"]
+        strat_vars_part = ["age_part"] + strat_vars
+        strat_vars_cnt = age_vars + strat_vars
 
     data = data[selected_vars]
 
@@ -171,41 +185,45 @@ def make_train_data(data: pd.DataFrame,
     data = data.dropna(axis=0)
     n_dropped = n_all - data.shape[0]
     if n_dropped > 0:
-        warnings.warn(f'Dropped {n_dropped} rows with missing values', RuntimeWarning)
+        warnings.warn(f"Dropped {n_dropped} rows with missing values", RuntimeWarning)
 
     # Convert non-numeric columns to categorical
-    non_numeric_cols = data[grp_vars_cnt].select_dtypes(include='object').columns
+    non_numeric_cols = data[strat_vars_cnt].select_dtypes(include="object").columns
     data = convert_to_categorical(data, non_numeric_cols)
 
     # Document the categories, intervals
     dict_cats = document_categories(data)
 
     # ===== Calculate N and y =====
-    df_N = data.groupby(grp_vars_part, observed=False).agg(
-        N=(id_var, 'nunique')).reset_index()
-    df_y = data.groupby(grp_vars_cnt, observed=False).agg(
-        y=('y', 'sum')).reset_index()
+    df_N = (
+        data.groupby(strat_vars_part, observed=False)
+        .agg(N=(id_var, "nunique"))
+        .reset_index()
+    )
+    df_y = (
+        data.groupby(strat_vars_cnt, observed=False).agg(y=("y", "sum")).reset_index()
+    )
 
     # ===== Create the grid =====
-    df_grid = make_full_grid(data, age_vars, grp_vars)
+    df_grid = make_full_grid(data, age_vars, strat_vars)
 
     # ===== Merge the dataframes =====
-    df = pd.merge(df_grid, df_y, on=grp_vars_cnt, how='left')
-    df = pd.merge(df, df_N, on=grp_vars_part, how='left')
+    df = pd.merge(df_grid, df_y, on=strat_vars_cnt, how="left")
+    df = pd.merge(df, df_N, on=strat_vars_part, how="left")
 
     # ===== Finalise the data =====
-    df.dropna(subset=['N'], inplace=True)
-    df = df[df['N'] != 0]
-    df['y'] = df['y'].fillna(0)
-    df['y'] = df['y'].astype(int)
-    df['N'] = df['N'].astype(int)
-    df['age_part'] = df['age_part'].astype(int)
-    if 'age_cnt' in df.columns:
-        df['age_cnt'] = df['age_cnt'].astype(int)
+    df.dropna(subset=["N"], inplace=True)
+    df = df[df["N"] != 0]
+    df["y"] = df["y"].fillna(0)
+    df["y"] = df["y"].astype(int)
+    df["N"] = df["N"].astype(int)
+    df["age_part"] = df["age_part"].astype(int)
+    if "age_cnt" in df.columns:
+        df["age_cnt"] = df["age_cnt"].astype(int)
 
     # Bring ['age_part', 'age_cnt'] to the front
-    age_part = df.pop('age_part')
-    df.insert(0, 'age_part', age_part)
+    age_part = df.pop("age_part")
+    df.insert(0, "age_part", age_part)
 
     age_cnt = df.pop(age_vars[1])
     df.insert(1, age_vars[1], age_cnt)
@@ -218,13 +236,15 @@ def make_train_data(data: pd.DataFrame,
     return df.reset_index(drop=True)
 
 
-def add_grp_cnt_offsets(df_cnt: pd.DataFrame,
-                        df_grp: pd.DataFrame,
-                        grp_vars: str | list[str] | None = None,
-                        max: int | None = None) -> pd.DataFrame:
+def add_grp_cnt_offsets(
+    df_cnt: pd.DataFrame,
+    df_grp: pd.DataFrame,
+    strat_vars: str | list[str] | None = None,
+    max: int | None = None,
+) -> pd.DataFrame:
     """Create offsets for group contacts and contacts with missing information.
 
-    Suppose grp_vars = ['sex_part']. Given the total number of contacts with complete information :math:`Y^g_a = sum_{b} Y^{g}_{a,b}`
+    Suppose strat_vars = ['sex_part']. Given the total number of contacts with complete information :math:`Y^g_a = sum_{b} Y^{g}_{a,b}`
     and the total number of contacts with missing information :math:`Z^g_a`, the offset for group contacts is calculated as
 
     ..math::
@@ -240,7 +260,7 @@ def add_grp_cnt_offsets(df_cnt: pd.DataFrame,
     df_grp : pd.DataFrame
         Data frame containing information for each individual and the number of group & contacts with missing information
         in a column named `z`.
-    grp_vars : str | list | None, optional
+    strat_vars : str | list | None, optional
         Column(s) to group by. default is None.
     max : int | None, optional
         Maximum number of group & missing contacts allowed. This is useful for avoiding offsets that are too large.
@@ -258,29 +278,40 @@ def add_grp_cnt_offsets(df_cnt: pd.DataFrame,
     >>> df_train = add_group_cnt_offsets(df_train, df_grp, 'sex_part', max=60)
     """
 
-    if 'y' not in df_cnt.columns:
-        warnings.warn('No column "y" found. Assuming each row represents a single contact.', RuntimeWarning)
-        df_cnt['y'] = 1
+    if "y" not in df_cnt.columns:
+        warnings.warn(
+            'No column "y" found. Assuming each row represents a single contact.',
+            RuntimeWarning,
+        )
+        df_cnt["y"] = 1
 
-    if 'z' not in df_grp.columns:
-        raise RuntimeError('No column "z" found. Please provide the number of group contacts in a column named "z"')
+    if "z" not in df_grp.columns:
+        raise RuntimeError(
+            'No column "z" found. Please provide the number of group contacts in a column named "z"'
+        )
 
-    if grp_vars is None:
-        grp_vars = ['age_part']
+    if strat_vars is None:
+        strat_vars = ["age_part"]
     else:
-        grp_vars = [grp_vars] if isinstance(grp_vars, str) else grp_vars
-        grp_vars = ['age_part'] + grp_vars
+        strat_vars = [strat_vars] if isinstance(strat_vars, str) else strat_vars
+        strat_vars = ["age_part"] + strat_vars
 
-    df_cnt_part = df_cnt.groupby(grp_vars, observed=True).agg({'y': 'sum'}).reset_index()
-    df_grp_sum = df_grp.groupby(grp_vars, observed=True).agg({'z': 'sum'}).reset_index()
+    df_cnt_part = (
+        df_cnt.groupby(strat_vars, observed=True).agg({"y": "sum"}).reset_index()
+    )
+    df_grp_sum = (
+        df_grp.groupby(strat_vars, observed=True).agg({"z": "sum"}).reset_index()
+    )
 
     if max is not None:  # Cap the sum of group contacts
-        df_grp_sum['z'] = df_grp_sum['z'].apply(lambda x: min(x, max))
+        df_grp_sum["z"] = df_grp_sum["z"].apply(lambda x: min(x, max))
 
-    df_offsets = pd.merge(df_cnt_part, df_grp_sum, on=grp_vars, how='left')
-    df_offsets['S'] = df_offsets['y'] / (df_offsets['y'] + df_offsets['z'])
-    df_offsets['S'] = df_offsets['S'].fillna(0)
-    df_offsets['S'] = np.where(df_offsets['S'] == 0, 1, df_offsets['S'])  # Avoid log(0) = -inf errors
+    df_offsets = pd.merge(df_cnt_part, df_grp_sum, on=strat_vars, how="left")
+    df_offsets["S"] = df_offsets["y"] / (df_offsets["y"] + df_offsets["z"])
+    df_offsets["S"] = df_offsets["S"].fillna(0)
+    df_offsets["S"] = np.where(
+        df_offsets["S"] == 0, 1, df_offsets["S"]
+    )  # Avoid log(0) = -inf errors
 
-    cols = grp_vars + ['S']
-    return pd.merge(df_cnt, df_offsets[cols], on=grp_vars, how='left')
+    cols = strat_vars + ["S"]
+    return pd.merge(df_cnt, df_offsets[cols], on=strat_vars, how="left")
